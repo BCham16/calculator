421b100dcd773141d4c4809994e29b70216ae342
BCham16 <bchamberlain.3618@gmail.com>
2022-06-05 01:23:54 -0400

    added keyboard

--- script.js
+++ script.js
@@ -1,84 +1,5 @@
 
 display = document.querySelector('#mainDisplay');
-num1 = document.querySelector('#num1');
-num2 = document.querySelector('#num2');
-num3 = document.querySelector('#num3');
-num4 = document.querySelector('#num4');
-num5 = document.querySelector('#num5');
-num6 = document.querySelector('#num6');
-num7 = document.querySelector('#num7');
-num8 = document.querySelector('#num8');
-num9 = document.querySelector('#num9');
-num0 = document.querySelector('#num0');
-plus = document.querySelector('#plus');
-minus = document.querySelector('#minus');
-times = document.querySelector('#times');
-equals = document.querySelector('#equals');
-divide = document.querySelector('#divide');
-clear = document.querySelector('#clear');
-
-num1.addEventListener('click', function(){
-  check() 
-  addToDisplay(1);}
-  )
-num2.addEventListener('click', function(){
-  check() 
-  addToDisplay(2);}
-  )
-num3.addEventListener('click', function(){
-  check() 
-  addToDisplay(3);}
-  )
-num4.addEventListener('click', function(){
-  check() 
-  addToDisplay(4);}
-  )
-num5.addEventListener('click', function(){
-  check() 
-  addToDisplay(5);}
-  )
-num6.addEventListener('click', function(){
-  check() 
-  addToDisplay(6);}
-  )
-num7.addEventListener('click', function(){
-  check()  
-  addToDisplay(7);}
-  )
-num8.addEventListener('click', function(){
-  check()  
-  addToDisplay(8);}
-  )
-num9.addEventListener('click', function(){
-  check()  
-  addToDisplay(9);}
-  )
-num0.addEventListener('click', function(){
-  check()  
-  addToDisplay(0);}
-  )
-plus.addEventListener('click', function(){
-  value1 = display.textContent
-  operand = 'add';
-  tracker = 1;
-})
-minus.addEventListener('click', function(){
-  value1 = display.textContent
-  operand = 'subtract';
-  tracker = 1;
-})
-times.addEventListener('click', function(){
-  value1 = display.textContent
-  operand = 'multiply';
-  tracker = 1;
-})
-divide.addEventListener('click', function(){
-  value1 = display.textContent
-  operand = 'divide';
-  tracker = 1;
-})
-equals.addEventListener('click', function(){value1 = complete(value1, operand)})
-clear.addEventListener('click', function(){clearAll()})
 
 let value1 = 0;
 let operand = '';
@@ -122,13 +43,6 @@ function clearAll(){
   clearDisplay()
 }
 
-// function first(){
-//   //if (value1 !== 0) complete(value1, operand);
-//   value1 = display.textContent
-//   //clearDisplay();
-//   return value1;
-// }
-
 function check(){
   if (tracker == 1){
     clearDisplay()
@@ -136,5 +50,146 @@ function check(){
   }
 }
 
-clearAll()
+function eventAssignment(){
+  document.querySelector('#num1').addEventListener('click', function(){
+    check() 
+    addToDisplay(1);}
+    )
+  document.querySelector('#num2').addEventListener('click', function(){
+    check() 
+    addToDisplay(2);}
+    )
+  document.querySelector('#num3').addEventListener('click', function(){
+    check() 
+    addToDisplay(3);}
+    )
+  document.querySelector('#num4').addEventListener('click', function(){
+    check() 
+    addToDisplay(4);}
+    )
+  document.querySelector('#num5').addEventListener('click', function(){
+    check() 
+    addToDisplay(5);}
+    )
+  document.querySelector('#num6').addEventListener('click', function(){
+    check() 
+    addToDisplay(6);}
+    )
+  document.querySelector('#num7').addEventListener('click', function(){
+    check()  
+    addToDisplay(7);}
+    )
+  document.querySelector('#num8').addEventListener('click', function(){
+    check()  
+    addToDisplay(8);}
+    )
+  document.querySelector('#num9').addEventListener('click', function(){
+    check()  
+    addToDisplay(9);}
+    )
+  document.querySelector('#num0').addEventListener('click', function(){
+    check()  
+    addToDisplay(0);}
+    )
+  document.querySelector('#plus').addEventListener('click', function(){
+    value1 = display.textContent
+    operand = 'add';
+    tracker = 1;
+  })
+  document.querySelector('#minus').addEventListener('click', function(){
+    value1 = display.textContent
+    operand = 'subtract';
+    tracker = 1;
+  })
+  document.querySelector('#times').addEventListener('click', function(){
+    value1 = display.textContent
+    operand = 'multiply';
+    tracker = 1;
+  })
+  document.querySelector('#divide').addEventListener('click', function(){
+    value1 = display.textContent
+    operand = 'divide';
+    tracker = 1;
+  })
+  document.querySelector('#equals').addEventListener('click', function(){
+    value1 = complete(value1, operand)
+  })
+  document.querySelector('#clear').addEventListener('click', function(){
+    clearAll()
+  })
+  document.addEventListener('keypress', function(x){
+    key = x.key
+    switch(key){
+      case '1':
+        check(); 
+        addToDisplay(1);
+        return;
+      case '2':
+        check(); 
+        addToDisplay(2);
+        return;
+      case '3':
+        check(); 
+        addToDisplay(3);
+        return;
+      case '4':
+        check(); 
+        addToDisplay(4);
+        return;
+      case '5':
+        check(); 
+        addToDisplay(5);
+        return;
+      case '6':
+        check(); 
+        addToDisplay(6);
+        return;
+      case '7':
+        check(); 
+        addToDisplay(7);
+        return;
+      case '8':
+        check(); 
+        addToDisplay(8);
+        return;
+      case '9':
+        check(); 
+        addToDisplay(9);
+        return;
+      case '0':
+        check(); 
+        addToDisplay(0);
+        return;
+      case '+':
+        value1 = display.textContent
+        operand = 'add';
+        tracker = 1;
+        return;
+      case '-':
+        value1 = display.textContent
+        operand = 'subtract';
+        tracker = 1;
+        return;
+      case '*':
+        value1 = display.textContent
+        operand = 'multiply';
+        tracker = 1;
+        return;
+      case '/':
+        value1 = display.textContent
+        operand = 'divide';
+        tracker = 1;
+        return;
+      case 'Enter':
+        value1 = complete(value1, operand);
+        return;
+      case 'c':
+        clearAll()
+        return;
+      default: return;
+    }
+  })
+}
 
+clearAll()
+eventAssignment()
